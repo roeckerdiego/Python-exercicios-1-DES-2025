0083a3308e68cce75fb99d7bc34dbcbaa9f14518
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-06-17 12:16:01 +0000

    terminado todos os exercicios.

--- exercicio012.py
+++ exercicio012.py
@@ -1,9 +1,11 @@
 #Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
 #Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
 
-senha = int(input("Digite sua senha."))
+senha = input("Digite sua senha, que contenha pelo menos 8 digitos.")
+
+digitos = len(senha)
 
 if senha >= 8:
-    print("Sua senha está valida.")
+    print("senha correta.")
 else:
-    print("Sua senha está invalida.")
+    print("senha incorreta.")
--- exercicio019.py
+++ exercicio019.py
@@ -1,4 +1,6 @@
 #Peça três números e exiba-os em ordem crescente.
 
-numeros = int(input("Digite três numeros"))
+numero1 = int(input("Digite o primeiro numero."))
+numero2 = int(input("Digite o segundo numero."))
+numero3 = int(input("Digite o terceiro numero."))
 
